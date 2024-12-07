91105e1125d738f4fe83b95dedcb026f26566436
JappeHallunken <jappe@posteo.de>
2024-12-07 05:17:03 +0100

    found a much faster solution

--- puzzles/day6/day6.go
+++ puzzles/day6/day6.go
@@ -2,7 +2,6 @@ package day6
 
 import (
 	// "fmt"
-	"fmt"
 	"slices"
 
 	"github.com/JappeHallunken/advent-of-code/fileops"
@@ -18,6 +17,13 @@ var directions = []point{ //-1,0 is top left
 	{0, 1},  // right
 	{1, 0},  // down
 	{0, -1}, // left
+}
+
+var newStart = []point{
+	{0, 1},  // start is 1 to the right
+	{1, 0},  // start is 1 down
+	{0, -1}, // start is 1 the left
+	{-1, 0}, // start is 1 up
 }
 
 var startDirectionIndex = 0
@@ -48,6 +54,8 @@ func getStartPoint(slice [][]rune) (startPoint point) {
 	return startPoint
 }
 
+var visitedPositions []point
+
 func findWays(input string) int {
 
 	slice := createRuneSlice(input)
@@ -60,9 +68,7 @@ func findWays(input string) int {
 
 	xLength := len(slice[0])
 	yLength := len(slice)
-
-	var visitedPositions []point
-	steps := 0 //starting position is a step
+	visitedPositions = append(visitedPositions, start)
 
 	// fmt.Println("dimensions: ", xLength, yLength)
 	// fmt.Println("start: ", start)
@@ -97,13 +103,13 @@ func findWays(input string) int {
 
 			position = nextPosition
 			if !slices.Contains(visitedPositions, position) {
-				steps++
 				visitedPositions = append(visitedPositions, position)
 			}
 		}
 		// fmt.Println("next position: ", position)
 	}
-	return steps
+	// fmt.Println(visitedPositions)
+	return len(visitedPositions)
 }
 
 func testForLoop(input string) (loopCount int) {
@@ -115,100 +121,78 @@ func testForLoop(input string) (loopCount int) {
 	start := getStartPoint(slice)
 	// fmt.Println("start: ", start)
 	loopcount := 0
-	for i := range slice {
-		for j := range slice[i] { // set every point in the [][]rune to '#' and see if we get a loop
-			var turningPos []point // record the turning positions, if they repeat we are in a loop
-			turningPosIdx := 0
-			position := start
-			directionsIndex := startDirectionIndex
-			direction := directions[directionsIndex]
 
-			// fmt.Println("i, j: ", i, j)
+	for _, p := range visitedPositions {
+		
+		// set every point in the [][]rune to '#' and see if we get a loop
+		// record the turning positions, if they repeat we are in a loop
+		// turningPosIdx := 0
+		position := start
+		directionsIndex := startDirectionIndex
+		direction := directions[directionsIndex]
 
-			if slice[i][j] == '#' {
+		var turningPos []point
+		if slice[p.x][p.y] == '#' {
+			continue
+		}
+
+		slice[p.x][p.y] = '#'
+
+		outsideArea, foundLoop := false, false
+		for !outsideArea || !foundLoop {
+
+			nextPosition := point{position.x + direction.x, position.y + direction.y}
+
+			if nextPosition.x < 0 || nextPosition.x >= xLength || nextPosition.y < 0 || nextPosition.y >= yLength {
+				// fmt.Println("###### leaving area ######", nextPosition)
+				outsideArea = true
+				break
+			}
+
+			if slice[nextPosition.x][nextPosition.y] == '#' {
+
+				directionsIndex = (directionsIndex + 1) % len(directions) // get next direction of directionsSlice, wrap around if necessary
+				direction = directions[directionsIndex]
+				turningPos = append(turningPos, position)
+
+				// fmt.Println("found obstacle", position)
+
 				continue
 			}
-
-			slice[i][j] = '#'
-
-			outsideArea, foundLoop := false, false
-			for !outsideArea || !foundLoop {
-
-				nextPosition := point{position.x + direction.x, position.y + direction.y}
-
-				if nextPosition.x < 0 || nextPosition.x >= xLength || nextPosition.y < 0 || nextPosition.y >= yLength {
-					// fmt.Println("leaving area")
-					outsideArea = true
-					break
-				}
-
-				if slice[nextPosition.x][nextPosition.y] == '#' {
-
-					directionsIndex = (directionsIndex + 1) % len(directions) // get next direction of directionsSlice, wrap around if necessary
-					direction = directions[directionsIndex]
-
-					turningPos = append(turningPos, position)
-
-					turningPosIdx = (turningPosIdx + 1)
-
-					//      fmt.Println("turnpoints: ", turningPos)
-					// fmt.Println("found obstacle")
-					// fmt.Println("new direction: ", direction)
-
-				
-
-					continue
-				}
-				if slice[nextPosition.x][nextPosition.y] != '#' {
-
-					position = nextPosition
-
-				}
-        if turningPosIdx > 2 && (hasDuplicates(turningPos[:len(turningPos)-2])) {
-					loopcount++
-					foundLoop = true	
-          // fmt.Println("loopcount: ", loopcount, "  ", i, j)
-
-          // fmt.Println("turning points: ", turningPos)
-          // fmt.Println("found loop at: ", i, j)
-					//      slice[position.x][position.y] = 'O'
-					// strings := make([][]string, len(slice))
-					// for i, row := range slice {
-					// 	strings[i] = make([]string, len(row))
-					// 	for j, r := range row {
-					// 		strings[i][j] = string(r)
-					// 	}
-					// }
-					// for _, row := range strings {
-					// 	fmt.Println(row)
-					// }
-					// fmt.Println()
-					// slice[position.x][position.y] = '.'
-					break
-				}
-				// fmt.Println("next position: ", position)
-
+			if compareLastWithSubset(turningPos) {
+				loopcount++
+				foundLoop = true
+				// fmt.Println("loopcount: ", loopcount, "  ", i, j)
+				// fmt.Println("turning points: ", turningPos)
+				// fmt.Println("found loop at: ", turningPos[len(turningPos)-1])
+				break
 			}
-			slice[i][j] = '.'
+
+			if slice[nextPosition.x][nextPosition.y] != '#' {
+				position = nextPosition
+			}
+			// fmt.Println(turningPos)
 		}
-    fmt.Println(i)
+		slice[p.x][p.y] = '.'
 	}
-  
+
 	return loopcount
 }
 
-func hasDuplicates(arr []point) bool {
-	seen := make(map[point]bool)
-	for _, v := range arr {
-		if seen[v] {
-			return true // duplicate found
-		}
-		seen[v] = true
+func compareLastWithSubset(arr []point) bool {
+	if len(arr) < 4 {
+		return false
 	}
-	return false // no duplicate
+
+	last := arr[len(arr)-1]    // Letztes Element
+	subset := arr[:len(arr)-3] // Alle außer den letzten drei Elementen
+	for _, v := range subset {
+		if v == last {
+			return true // Das letzte Element ist in diesem Subset vorhanden
+		}
+	}
+	return false // Kein Duplikat gefunden
 }
-
-
 
 func Day6(input string) (score1, score2 int) {
 
@@ -216,6 +200,7 @@ func Day6(input string) (score1, score2 int) {
 	score1 = findWays(input)
 
 	// puzzle 2
-  score2 = testForLoop(input)
-  return score1, score2
+	score2 = testForLoop(input)
+
+	return score1, score2
 }
--- puzzles/day6/day6_test.go
+++ puzzles/day6/day6_test.go
@@ -1,31 +1,27 @@
 package day6
 
 import (
-	"fmt"
 	"testing"
-
 )
 
 func TestDay5(t *testing.T) {
 	inputFile := "../../input/day6_test.txt"
-	
+
 	t.Run("Test Day 6 Puzzle 1", func(t *testing.T) {
-  expectedResult := 41
-  result :=	findWays(inputFile)
+		expectedResult := 41
+		result := findWays(inputFile)
 		if result != expectedResult {
 			t.Errorf("Erwartetes Ergebnis: %d, erhalten: %d", expectedResult, result)
 
 		}
 	})
 	t.Run("Test Day 5 Puzzle 2", func(t *testing.T) {
-    fmt.Println("TBD")
-		//
-		// expectedResult := 123
-		// fixedInvalidNumbers := fixOrder(rules, invalidNumbers)
-		// result := findMiddleAndSum(fixedInvalidNumbers)
-		// if result != expectedResult {
-		// 	t.Errorf("Erwartetes Ergebnis: %d, erhalten: %d", expectedResult, result)
-		// }
+		expectedResult := 6
+		result := testForLoop(inputFile)
+
+		if result != expectedResult {
+			t.Errorf("Erwartetes Ergebnis: %d, erhalten: %d", expectedResult, result)
+		}
 	})
 
 }

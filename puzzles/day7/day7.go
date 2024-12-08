f3677555d824335bc04ca7b43d4f8e71e906115a
JappeHallunken <jappe@posteo.de>
2024-12-08 06:14:47 +0100

    tidy up

--- puzzles/day7/day7.go
+++ puzzles/day7/day7.go
@@ -38,13 +38,13 @@ func makeSlice(input string) (twoDIntSlice [][]uint64) {
 		}
 		twoDIntSlice = append(twoDIntSlice, intSlice)
 	}
-	fmt.Println(twoDIntSlice)
+	// fmt.Println(twoDIntSlice)
 	return twoDIntSlice
 }
 
 func findOperators(equations [][]uint64) (pass2Ops, pass3Ops, combinedLines []int) { //finds the valid lines and returns the idx of the lines
 	for r := range equations {
-		fmt.Printf("%v of %v\n", r+1, len(equations))
+		// fmt.Printf("%v of %v\n", r+1, len(equations))
 
 		if tryOperators(equations[r]) { // 2 operators
 			pass2Ops = append(pass2Ops, r)
@@ -54,7 +54,7 @@ func findOperators(equations [][]uint64) (pass2Ops, pass3Ops, combinedLines []in
 			if (findCombinations(equations[r])) { // 3 operators
 				// fmt.Println("check valid Invald: ", equations[r])
 				pass3Ops = append(pass3Ops, r)
-        fmt.Println("add line: ", r)
+        // fmt.Println("add line: ", r)
 				combinedLines = append(combinedLines, r)
 			}
 		}
@@ -85,11 +85,11 @@ func tryOperators(nums []uint64) bool {
 		}
 		result := calculate(nums, operators)
 		if result == target {
-			fmt.Printf("true: ")
-			fmt.Println("result: ", result)
-			fmt.Println("nums: ", nums)
-			fmt.Println("operators: ", string(operators))
-			printEquation(nums, operators, result)
+			// fmt.Printf("true: ")
+			// fmt.Println("result: ", result)
+			// fmt.Println("nums: ", nums)
+			// fmt.Println("operators: ", string(operators))
+			// printEquation(nums, operators, result)
 			return true
 		}
 		// printEquation(nums, operators, result)
@@ -116,7 +116,7 @@ func printEquation(nums []uint64, operators []rune, result uint64) {
 	for i, operator := range operators {
 		equation += string(operator) + strconv.FormatUint(nums[i+2], 10)
 	}
-	fmt.Printf("Equation: %v = %v\n", strconv.FormatUint(result, 10), equation)
+	// fmt.Printf("Equation: %v = %v\n", strconv.FormatUint(result, 10), equation)
 }
 
 func calculateSum(validLines []int, equations [][]uint64) uint64 {
@@ -173,8 +173,8 @@ func findCombinations(numbers []uint64) bool {
 		// Evaluate and compare
     result := evaluate(numbers[1:], comb)
 		if result == target {
-			fmt.Printf("Match found with operators: %v\n", string(comb))
-			fmt.Printf("%v = %v with operators %v\n", target, numbers, string(comb))
+			// fmt.Printf("Match found with operators: %v\n", string(comb))
+			// fmt.Printf("%v = %v with operators %v\n", target, numbers, string(comb))
 			return true
 		} else {
 			// fmt.Printf("No match for combination: %v, result: %v\n", string(comb), result)
@@ -188,9 +188,9 @@ func Day7(input string) (uint64, uint64, uint64) {
 	// puzzle 1
 	slice := makeSlice(input)
 	pass2, pass3, combined := findOperators(slice)
-	fmt.Println("valid lines: ", pass2)
-	fmt.Println("valid invlid lines: ", pass3)
-
+	// fmt.Println("valid lines: ", pass2)
+	// fmt.Println("valid invlid lines: ", pass3)
+	//
 	sum := calculateSum(pass2, slice) // with 2 operators
 
 	// puzzle 2
@@ -198,10 +198,10 @@ func Day7(input string) (uint64, uint64, uint64) {
 	sum2 := calculateSum(pass3, slice)    // pass 3 Operators
 	sum3 := calculateSum(combined, slice) // pass 2 and 3
 
-	fmt.Println("sum: ", sum)
-	fmt.Println("sum2: ", sum2)
-	fmt.Println("sum3: ", sum3)
-	fmt.Println(sum+sum2 == sum3)
+	// fmt.Println("sum: ", sum)
+	// fmt.Println("sum2: ", sum2)
+	// fmt.Println("sum3: ", sum3)
+	// fmt.Println(sum+sum2 == sum3)
 
 	return sum, sum2, sum3
 }
--- puzzles/puzzles.go
+++ puzzles/puzzles.go
@@ -3,37 +3,36 @@ package puzzles
 import (
 	// "fmt"
 
-	// "github.com/JappeHallunken/advent-of-code/puzzles/day1"
-	// "github.com/JappeHallunken/advent-of-code/puzzles/day2"
-	// "github.com/JappeHallunken/advent-of-code/puzzles/day3"
-	// "github.com/JappeHallunken/advent-of-code/puzzles/day4"
-	// "github.com/JappeHallunken/advent-of-code/puzzles/day5"
-	// "github.com/JappeHallunken/advent-of-code/puzzles/day6"
+	"github.com/JappeHallunken/advent-of-code/puzzles/day1"
+	"github.com/JappeHallunken/advent-of-code/puzzles/day2"
+	"github.com/JappeHallunken/advent-of-code/puzzles/day3"
+	"github.com/JappeHallunken/advent-of-code/puzzles/day4"
+	"github.com/JappeHallunken/advent-of-code/puzzles/day5"
+	"github.com/JappeHallunken/advent-of-code/puzzles/day6"
 	"fmt"
 
 	"github.com/JappeHallunken/advent-of-code/puzzles/day7"
 )
 
 func SolveAll() {
-	// d1p1, d1p2 := day1.Day1("./input/day1.txt")
-	// fmt.Printf("\nDay 1 puzzle 1: %v\n      puzzle 2: %v \n------------------------------\n\n", d1p1, d1p2)
-	//
-	// d2p1, d2p2 := day2.Day2("./input/day2.txt")
-	// fmt.Printf("Day 2 puzzle 1: %v\n      puzzle 2: %v \n------------------------------\n\n", d2p1, d2p2)
-	//
-	// d3p1, d3p2 := day3.Day3("./input/day3.txt")
-	// fmt.Printf("Day 3 puzzle 1: %v\n      puzzle 2: %v \n------------------------------\n\n", d3p1, d3p2)
-	//
-	// d4p1, d4p2 := day4.Day4("./input/day4.txt")
-	// fmt.Printf("Day 4 puzzle 1: %v\nDay 4 puzzle 2: %v \n------------------------------\n\n", d4p1, d4p2)
-	//
-	//  d5p1, d5p2 := day5.Day5("./input/day5.txt")
-	// fmt.Printf("Day 5 puzzle 1: %v\nDay 5 puzzle 2: %v \n------------------------------\n\n", d5p1, d5p2)
-	//
-	//  d6p1, d6p2 := day6.Day6("./input/day6.txt")
-	// fmt.Printf("Day 6 puzzle 1: %v\nDay 6 puzzle 2: %v \n------------------------------\n\n", d6p1, d6p2 )
+	d1p1, d1p2 := day1.Day1("./input/day1.txt")
+	fmt.Printf("\nDay 1 puzzle 1: %v\n      puzzle 2: %v \n------------------------------\n\n", d1p1, d1p2)
 
-	d7p1, d7p2, sum := day7.Day7("./input/day7.txt")
-  fmt.Printf("Day 7 puzzle 1: %v\nDay 7 puzzle 2: %v \nSum: %v \n------------------------------\n\n", d7p1, d7p2, sum)
-	 // fmt.Println(d7p1)
+	d2p1, d2p2 := day2.Day2("./input/day2.txt")
+	fmt.Printf("Day 2 puzzle 1: %v\n      puzzle 2: %v \n------------------------------\n\n", d2p1, d2p2)
+
+	d3p1, d3p2 := day3.Day3("./input/day3.txt")
+	fmt.Printf("Day 3 puzzle 1: %v\n      puzzle 2: %v \n------------------------------\n\n", d3p1, d3p2)
+
+	d4p1, d4p2 := day4.Day4("./input/day4.txt")
+	fmt.Printf("Day 4 puzzle 1: %v\nDay 4 puzzle 2: %v \n------------------------------\n\n", d4p1, d4p2)
+
+	 d5p1, d5p2 := day5.Day5("./input/day5.txt")
+	fmt.Printf("Day 5 puzzle 1: %v\nDay 5 puzzle 2: %v \n------------------------------\n\n", d5p1, d5p2)
+
+	 d6p1, d6p2 := day6.Day6("./input/day6.txt")
+	fmt.Printf("Day 6 puzzle 1: %v\nDay 6 puzzle 2: %v \n------------------------------\n\n", d6p1, d6p2 )
+
+	d7p1, _, sum := day7.Day7("./input/day7.txt")
+  fmt.Printf("Day 7 puzzle 1: %v\nDay 7 puzzle 2: %v \n------------------------------\n\n", d7p1, sum)
 }
